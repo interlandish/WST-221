80f5f2ab0460e679fd94385b59a64f482f669a24
Interlandish <43921072+interlandish@users.noreply.github.com>
2019-07-18 11:15:44 +0200

    Update Practical 1.r

--- Practical 1.r
+++ Practical 1.r
@@ -35,3 +35,33 @@ data <- data.frame(data, list)
 
 lines(data$list, data$hp)
 
+# Question 4
+
+quad <- function(a, b, c)
+{
+  if(a == 0)
+  {
+    print("The equation is not a quadratic equation")
+  }
+  else
+  {
+    desc <- (b^2 - 4 * a * c)
+    if(desc < 0)
+    {
+      print("The equation has no real roots")
+    }
+    else if(desc == 0)
+    {
+      root <- (-b)*(2*a)
+      print(paste("You equation has a double root: ", root))
+    }
+    else if(desc > 0)
+    {
+      root1 <- (-b + sqrt(desc))/(2 * a)
+      root2 <- (-b - sqrt(desc))/(2 * a)
+      print(paste("Your equation has two real roots: ", root1, " and ", root2))
+    }
+  }
+}
+
+quad(1, 3, 1)
