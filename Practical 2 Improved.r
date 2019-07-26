1c85a45868ad83a4c502c10dead55a42f787eba2
Interlandish <43921072+interlandish@users.noreply.github.com>
2019-07-26 10:16:25 +0200

    Create Practical 2 Improved.r

---
+++ Practical 2 Improved.r
@@ -0,0 +1,57 @@
+library(ggplot2)
+library(qqplotr)
+
+normalSample <- rnorm(10000, mean = 5, sd = 5)
+binomialSample <- rbinom(10000, 5, 0.7)
+betaSample <- rbeta(10000, 0.5, 0.5)
+
+hist(normalSample, main = "Normal Sample")
+hist(binomialSample, main = "Binomial Sample")
+hist(betaSample, main = "Beta Sample")
+
+# Generating the random samples
+normalSamples1 = matrix(rnorm(10*1000, 5, 5), ncol = 1000, nrow = 10)
+normalSamples2 = matrix(rnorm(1000*1000, 5, 5),ncol = 1000, nrow = 1000)
+
+binomialSamples1 = matrix(rbinom(10*1000, 5, 0.7), ncol = 1000, nrow = 10)
+binomialSamples2 = matrix(rbinom(1000*1000, 5, 0.7), ncol = 1000, nrow = 1000)
+
+betaSample1 = matrix(rbeta(10*1000, 0.5, 0.5), ncol = 1000, nrow = 10)
+betaSample2 = matrix(rbeta(1000*1000, 0.5, 0.5), ncol = 1000, nrow = 1000)
+
+generateZn <- function(matrix, size, mean, variance)
+{
+  znVector <- numeric()
+  for(i in 1:1000)
+  {
+    sumXi <- sum(matrix[, i])
+    mu <- size * mean
+    sd <- sqrt(size) * sqrt(variance)
+    zn <- (sumXi - mu)/sd
+    znVector <- append(znVector, zn, i)
+  }
+  return(znVector)
+  
+}
+
+normalZn1 <- generateZn(normalSamples1, 10, 5, 25)
+normalZn2 <- generateZn(normalSamples2, 1000, 5, 25)
+
+binomialZn1 <- generateZn(binomialSamples1, 10, 0.7 * 5, 0.7 * 5 * 0.3)
+binomialZn2 <- generateZn(binomialSamples2, 1000, 0.7 * 5, 0.7 * 5 * 0.3)
+
+betaZn1 <- generateZn(betaSample1, 10, 0.5, 0.25 / 2)
+betaZn2 <- generateZn(betaSample2, 1000, 0.5, 0.25 / 2)
+
+hist(normalZn2)
+hist(binomialZn2)
+hist(betaZn2)
+
+qqnorm(normalZn2, main = "Normal Zn")
+qqline(normalZn2)
+
+qqnorm(binomialZn2, main = "Normal Zn")
+qqline(binomialZn2)
+
+qqnorm(betaZn2, main = "Normal Zn")
+qqline(betaZn2)
