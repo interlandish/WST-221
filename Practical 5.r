c5af9e91ac7b2776360ff568267ba656fb20b8ac
Interlandish <43921072+interlandish@users.noreply.github.com>
2019-08-30 11:47:28 +0200

    Update Practical 5.r

--- Practical 5.r
+++ Practical 5.r
@@ -1,8 +1,11 @@
+library(qqplotr)
+library(car)
+
 #Question 1
 
 xbar = vector()
 y1vec = vector()
-tvec - vector()
+tvec = vector()
 
 nsim = 1000
 n = 1000
@@ -17,17 +20,17 @@ for(i in 1:nsim)
 
 #bias 
 print("Bias of Xbar:")
-print(abs(mean(xbar)))
+print(abs(mean(xbar)) - 5)
 
 print("Bias of T:")
-print(abs(mean(tvec)))
+print(abs(mean(tvec)) - 5)
 
 #verify dist of y1
 print("Mean of y1")
 print(mean(y1vec))
 
-hist(y1vec, probability = TRUE, 50)
-lines(swq(0, 0.04, by = 0.001), dexp(seq(0,0.04, by = 0.001)))
+hist(y1vec, 50, probability = TRUE)
+lines(seq(0, 0.04, by = 0.001), dexp(seq(0,0.04, by = 0.001), 1/5))
 
 #1.3
 
@@ -68,8 +71,59 @@ print(rexbat)
 eff = 1/rexbat
 print(eff)
 
-
 #Question 2
+
+
+vartn = vector()
+plotvec = vector()
+
+for(i in seq(50, 10000, by = 100))
+{
+  tnvec = vector()
+  for(j in 1:1000)
+  {
+    x = rexp(i, 1/5)
+    tnvec[j] = 1/mean(x)
+  }
+  vartn[i] = var(tnvec)
+  plotvec[i] = i  #sample size
+}
+
+plot(plotvec, vartn)
+# def 9.4.2
+
+#crlb = 1/(n * theta^2)
+
+crlb = vector()
+theta = 5
+for(i in seq(50, 10000, by = 100))
+{
+  crlb[i] = (1)/(i * theta^2)
+}
+
+ratiovec = crlb/vartn
+
+plot(plotvec, ratiovec)
+
+#2.6
+rnvec = vector()
+for(i in 1:50000)
+{
+  x = rexp(5000, 1/5)
+  rnvec[i] =  exp(-1/mean(x))
+}
+
+theta = 5
+t  = 0.1
+
+empmean = mean(rnvec)
+theoretical = exp(-t/theta)
+
+empvar = var(rnvec)
+theoreticalvar = (exp(-t/theta)*(t/theta^2)^2)*(theta^2/50000)
+
+hist(rnvec, 100, probability = TRUE)
+lines(seq(0.8, 0.83, 0.001), dnorm(seq(0.8, 0.83, 0.001), theoretical, sqrt(theoreticalvar)), lwd = 2)
 
 
 
