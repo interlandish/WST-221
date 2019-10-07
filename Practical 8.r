6f7a582f7ac985c4f124802d25cfb7b35474b516
Interlandish <43921072+interlandish@users.noreply.github.com>
2019-10-07 15:51:59 +0200

    Update Practical 8.r

--- Practical 8.r
+++ Practical 8.r
@@ -40,3 +40,45 @@ powern = pchisq(chivec/2, dfvector, lower.tail = FALSE)
 
 plot(powern ~ nvec, xlab = "x", ylab = "Power", col = "blue", lwd = 8, pch = 1, ylim = c(min(powern), max(powern)), main = "Power as a function of n")
 
+
+#Question 2
+
+mu0 = 6
+alpha = 0.05
+sigma = 10
+mu2 = 5.8
+beta = 0.1
+z1mina = qnorm(1-alpha)
+z1minb = qnorm(1 - beta)
+
+muvec = seq(5.9, 7.1, 0.1)
+nvec1 =(sigma*(z1minb - z1mina)^2)/((muvec - mu2)^2) 
+
+plot(nvec1 ~ muvec, 
+     type = "b", 
+     ylab = "n", 
+     xlab = "Mu0", 
+     col = "blue", 
+     lwd = 3, 
+     pch = 5, 
+     ylim = c(min(nvec1), max(nvec1)), 
+     main = "Sample size influenced by n")
+#2
+sigmavec = seq(5, 20, 1)
+nvec2 = (sigmavec*(z1minb - z1mina)^2)/((mu0 - mu2)^2) 
+
+plot(nvec1 ~ sigmavec, 
+     type = "b", 
+     ylab = "n", 
+     xlab = "Mu0", 
+     col = "blue", 
+     lwd = 3, 
+     pch = 5, 
+     ylim = c(min(nvec1), max(nvec1)), 
+     main = "Sample size influenced by n")
+#3
+alphavec = seq(0.01, 0.1, 0.01)
+zvec1 = qnorm(1 - alphavec)
+nvec3 = (sigmavec*(z1minb - z1mina)^2)/((mu0 - mu2)^2) 
+
+
