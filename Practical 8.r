a540c67c3a9a37bea52ffe3f145d5e3374647bd6
Interlandish <43921072+interlandish@users.noreply.github.com>
2019-10-07 14:53:18 +0200

    Create Practical 8.r

---
+++ Practical 8.r
@@ -0,0 +1,28 @@
+#Question 1.2
+
+thetavec = seq(0.3, 3, 0.1)
+
+powerA = pchisq(37.57/thetavec, 20, lower.tail = FALSE)
+powerB = pchisq(32.42/thetavec, 20, lower.tail = FALSE)
+powerC = pchisq(55.76/thetavec, 40, lower.tail = FALSE)
+
+plot(powerA ~ thetavec, type = "b", 
+     bty = "l", 
+     xlab = "Theta", 
+     ylab = "Power", 
+     col = "blue", 
+     lwd = 3, 
+     pch = 5, 
+     xlim = c(0.3, 3), 
+     main = "Power of the test")
+
+lines(powerB ~ thetavec, col = "red", lwd = 3, plch = 18, type = "b")
+lines(powerC ~ thetavec, col = "green", lwd = 3, plch = 14, type = "b")
+legend("bottomright", legend = c("Test 1", "Test B", "Test c"), 
+                                col = c("blue", "red", "green"),
+                                pch = c(5, 18, 14), 
+                                pt.cex = 2, 
+                                cex = 1.2, 
+                                text.col = "black", 
+                                horiz = F, 
+                                inset = c(0.1, 0.1))
